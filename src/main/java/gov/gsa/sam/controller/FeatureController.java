ea2923ecec0ba1b08c9781a2c1b7b219900f3242
sumitdang <sumit.dang@octoconsulting.com>
2017-10-05 14:38:06 -0400

    Added MediaType

--- src/main/java/gov/gsa/sam/controller/FeatureController.java
+++ src/main/java/gov/gsa/sam/controller/FeatureController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +20,7 @@ import gov.gsa.sam.repository.ToggleRepository;
 import io.swagger.annotations.ApiParam;
 
 @RestController
-@RequestMapping("/feature/v1")
+@RequestMapping(value = "/feature/v1", produces = MediaType.APPLICATION_JSON_VALUE)
 public class FeatureController {
 	@Autowired
 	ToggleRepository toggleRepo;
